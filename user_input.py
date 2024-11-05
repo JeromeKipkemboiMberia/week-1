7a9e034c17d3c172ffcb0c44a6fa7ec892ea3949
Jerome Mberia <jeromekipkemboimberia+github@gmail.com>
2024-11-05 17:55:19 +0300

    Add python code.

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("Enter your Name: ")
+age = input("Enter your Age: ")
+location = input("Enter your Location: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
